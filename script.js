c4216b4f5074771d87551a3dea31d773fc7b8c35
Gabriel <160670197+Gdhjjsjhd@users.noreply.github.com>
2024-05-06 08:26:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const estilos = ['newspaper', 'magazine1', 'magazine2', 'style1', 'style2', 'style3', 'style4', 'style5', 'style6', 'style7', 'style8']
+
+const tamanhos = ['medium', 'big', 'reaallybig']
+
+const rotacoes = ['rotateleft', 'rotateright']
+
+const inclinacoes = ['skewleft', 'skewright']
+
+const cores = [ 'red', 'blue', 'orange', 'green', 'purple', 'yellow', 'pink']
