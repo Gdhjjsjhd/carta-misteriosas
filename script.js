34351193858e05bbe9c22bb4ddfc0327c65b6cab
Gabriel <160670197+Gdhjjsjhd@users.noreply.github.com>
2024-05-07 14:18:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,61 @@ const rotacoes = ['rotateleft', 'rotateright']
 const inclinacoes = ['skewleft', 'skewright']
 
 const cores = [ 'red', 'blue', 'orange', 'green', 'purple', 'yellow', 'pink']
+
+function gerarCarta(){
+    const p = document.getElementById('carta-gerada');
+    p.innerHTML='';
+
+    const texto = document.getElementById('carta-texto').value.trim();
+
+    if(!texto){
+        p.textContent = 'Por favor, digite o conteúdo da carta! 🐒'
+        return;
+    }
+
+    const letras = texto.split('')
+    const contador = document.getElementById('carta-contador');
+
+    contador.textContent = letras.length;
+
+    letras.forEach((letra) => {
+
+        const span = document.createElement('span');
+        span.textContent = letra;
+        span.classList.add(
+            gerarClasseAleatoria(estilos),
+            gerarClasseAleatoria(tamanhos),
+            gerarClasseAleatoria(rotacoes),
+            gerarClasseAleatoria(inclinacoes),
+            gerarClasseAleatoria(cores)
+        );
+
+
+        span.addEventListener('click' , (event) => {
+            const spanClicado  = event.target;
+            const classesAtuais = spanClicado.classList
+            const proximoEstilo = gerarClasseAleatoria(estilos)
+
+
+            classesAtuais.remove(...estilos);
+            classesAtuais.add(proximoEstilo)
+
+    
+
+
+        })
+        
+        p.appendChild(span)
+    });
+
+}
+
+
+
+    const botao = document.getElementById('criar-carta');
+    botao.addEventListener('click', gerarCarta);
+
+    function  gerarClasseAleatoria(opcoes){
+        return opcoes [Math.floor(Math.random() * opcoes.length)]
+    }
+
